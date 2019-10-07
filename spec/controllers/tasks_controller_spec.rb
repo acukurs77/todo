d487485f881597e6e84d0b56aac4a7ac741fe00d
acukurs77 <ariana.cukurs1@gmail.com>
2019-10-07 20:55:18 +0000

    spec failure

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
 	describe "tasks#index" do
-		it "should ist the task in the database" do
+		it "should list the task in the database" do
 			task1 = FactoryBot.create(:task)
 			task2 = FactoryBot.create(:task)
 			task1.update_attributes(title: "Something else")
@@ -24,6 +24,16 @@ RSpec.describe TasksController, type: :controller do
 			expect(response).to have_http_status(:success)
 			task.reload
 			expect(task.done).to eq(true)
+		end
+	end
+
+	describe "tasks#create" do
+		it "should allow new tasks to be created" do
+			post :create, params: {task: {title: "Fix things"}}
+      		expect(response).to have_http_status(:success)
+      		response_value = ActiveSupport::JSON.decode(@response.body)
+      		expect(response_value['title']).to eq("Fix things")
+      		expect(Task.last.title).to eq("Fix things")
 		end
 	end
 end
